64a73f36fbe6e68a13748303cd6cbeae9c3406f8
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-10-10 17:47:28 +0500

    Update Models and Filament Resources

---
+++ database/migrations/2024_10_10_071206_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedInteger('author_id')->nullable();
+            $table->foreignId('category_id')->nullable();
+            $table->string('status');
+            $table->string('featured_image')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->softDeletes('deleted_at');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
