748eb23fb8e9487145f4d2541212ce2575b31487
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-10-17 13:54:53 +0500

    Create footer.blade for every blades

--- resources/views/frontend/hujjatlar/vazir_qonun.blade.php
+++ resources/views/frontend/hujjatlar/vazir_qonun.blade.php
@@ -793,14 +793,9 @@
 </div>
 
 
-<div class="site-footer">
-    <footer class="site-info" aria-label="Сайт" itemtype="https://schema.org/WPFooter" itemscope>
-        <div class="inside-site-info grid-container grid-parent">
-            <div class="copyright-bar">
-                <span class="copyright">&copy; 2024 TEXNIK JIHATDAN TARTIBGA SOLISH SOHASIDA NAZORAT INSPEKSIYASI</span></div>
-        </div>
-    </footer>
-</div>
+{{--footer qismi--}}
+@include('partials.footer')
+
 
 <script id="generate-a11y">
     !function () {
--- resources/views/frontend/inspeksiya_haqida/rahbariyat.blade.php
+++ resources/views/frontend/inspeksiya_haqida/rahbariyat.blade.php
@@ -103,15 +103,9 @@
     </div>
 </div>
 
+{{--footer qismi--}}
+@include('partials.footer')
 
-<div class="site-footer">
-    <footer class="site-info" aria-label="Сайт" itemtype="https://schema.org/WPFooter" itemscope>
-        <div class="inside-site-info grid-container grid-parent">
-            <div class="copyright-bar">
-                <span class="copyright">&copy; 2024 TEXNIK JIHATDAN TARTIBGA SOLISH SOHASIDA NAZORAT INSPEKSIYASI</span> </div>
-        </div>
-    </footer>
-</div>
 
 <script id="generate-a11y">
     !function () {
