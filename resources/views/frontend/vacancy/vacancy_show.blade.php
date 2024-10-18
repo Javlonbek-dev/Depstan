67da79cb1a13d8c6ed108a8597192d8ffc785550
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-10-18 11:52:16 +0500

    Updated vacancy blades

---
+++ resources/views/frontend/vacancy/vacancy_show.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+<title>Тадбиркорлик субъектларига ёрдам кўрсатиш бўлимида бош мутахассис лавозими &#8212; TEXNIK JIHATDAN TARTIBGA SOLISH SOHASIDA NAZORAT INSPEKSIYASI</title>
+@include('partials.header')
+
+<body class="post-template-default single single-post postid-1512 single-format-standard wp-custom-logo wp-embed-responsive post-image-above-header post-image-aligned-center sticky-menu-fade mobile-header mobile-header-logo mobile-header-sticky right-sidebar nav-below-header one-container fluid-header active-footer-widgets-3 nav-search-enabled nav-aligned-left header-aligned-left dropdown-hover elementor-default elementor-kit-12"
+      itemtype="https://schema.org/Blog" itemscope>
+<a class="screen-reader-text skip-link" href="#content" title="Перейти к содержимому">Перейти к содержимому</a>
+@include('partials.menu-header')
+@include('partials.menu')
+<div class="site grid-container container hfeed grid-parent" id="page">
+    <div class="site-content" id="content">
+
+        <div class="content-area grid-parent mobile-grid-100 grid-75 tablet-grid-75" id="primary">
+            <main class="site-main" id="main">
+
+                <article id="post-1512" class="post-1512 post type-post status-publish format-standard hentry category-bosh-ish-orinlari" itemtype="https://schema.org/CreativeWork" itemscope>
+                    <div class="inside-article">
+                        <header class="entry-header">
+                            <h1 class="entry-title" itemprop="headline">Тадбиркорлик субъектларига ёрдам кўрсатиш бўлимида бош мутахассис лавозими</h1>
+                            <div class="entry-meta">
+                                <span class="posted-on"><time class="updated" datetime="2021-03-29T17:28:10+05:00" itemprop="dateModified">29.03.2021</time><time class="entry-date published" datetime="2021-03-27T10:02:00+05:00" itemprop="datePublished">27.03.2021</time></span>                                    </div>
+                        </header>
+
+                        <div class="entry-content" itemprop="text">
+
+                            <figure class="wp-block-table">
+                                <table>
+                                    <tbody>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Номи</td>
+                                        <td>Тадбиркорлик субъектларига ёрдам кўрсатиш бўлимида бош мутахассис лавозими, &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Ташкилот: &nbsp;</td>
+                                        <td>Tехник жиҳатдан тартибга солиш, стандартлаштириш, сертификатлаштириш, ва метрология соҳасидаги қонунчилик талабларига риоя қилинишини таъминлаш Департаменти. &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Чоп этилган сана: &nbsp;</td>
+                                        <td>2021-03-27</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Бўлим: &nbsp;</td>
+                                        <td>Тадбиркорлик субъектларига ёрдам кўрсатиш бўлими, &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Маош: &nbsp;</td>
+                                        <td>1.790.000 &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Мутахассислик: &nbsp;</td>
+                                        <td>Ходим Ўзбекистон Республикаси Президентининг “Техник жиҳатдан тартибга солиш, стандартлаштириш, сертификатлаштириш ва метрология тизимларини янада ривожлантириш чора-тадбирлари тўғрисида”ги 2018 йил 12 декабрдаги
+                                            ПҚ-4059-сон ҳамда Ўзбекистон Республикаси Вазирлар Маҳкамасининг 2019 йил 24 апрелдаги ВМ-348-сон қарорлари, бошқа&nbsp;&nbsp; қонун ва норматив ҳужжатларга асосланиб иш фаолиятини олиб боради. Техник
+                                            жиҳатдан тартибга солиш, стандартлаштириш, сертификатлаштириш ва метрология&nbsp; ҳамда АКТ соҳасида ҳам тажрибаси бўлиши лозим &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Талаб этиладиган иш тажрибаси: &nbsp;</td>
+                                        <td>3-5&nbsp; йил &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Бандлик: &nbsp;</td>
+                                        <td>Тўлиқ: &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Маълумоти: &nbsp;</td>
+                                        <td>Олий &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="has-text-align-left" data-align="left">Жинси: &nbsp;</td>
+                                        <td>&#8212;-</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </figure>
+                        </div>
+
+                        <footer class="entry-meta" aria-label="Мета записи">
+                            <span class="cat-links"><span class="screen-reader-text">Рубрики </span><a href="https://depstan.uz/category/bosh-ish-orinlari/" rel="category tag">Бўш иш ўринлари</a></span>
+                            <nav id="nav-below" class="post-navigation"
+                                 aria-label="Записи">
+                            </nav>
+                        </footer>
+                    </div>
+                </article>
+            </main>
+        </div>
+
+        <div class="widget-area sidebar is-right-sidebar grid-25 tablet-grid-25 grid-parent" id="right-sidebar">
+            <div class="inside-right-sidebar">
+
+                <aside id="recent-posts-2" class="widget inner-padding widget_recent_entries">
+                    <h2 class="widget-title">Сўнгги янгиликлар</h2>
+                    <ul>
+                        <li>
+                            <a href="https://depstan.uz/2022/11/22/yangi-mehnat-kodeksidagi-ozgarishlar/">Янги Меҳнат Кодексидаги ўзгаришлар</a>
+                        </li>
+                        <li>
+                            <a href="https://depstan.uz/2022/11/22/2826/">O&#8217;zbekiston Respublikasi Mehnat Kodeksiga kiritilgan o&#8217;zgartirishlar</a>
+                        </li>
+                        <li>
+                            <a href="https://depstan.uz/2022/08/30/oziq-ovqat-mahsulotlarini-tamgalashga-qoyilgan-talablar/">OZIQ OVQAT MAHSULOTLARINI TAMG`ALASHGA QO`YILGAN TALABLAR…</a>
+                        </li>
+                        <li>
+                            <a href="https://depstan.uz/2022/08/10/importni-ornini-bosuvchi-eksportbob-mahsulotlar-organildi/">IMPORTNI O`RNINI BOSUVCHI EKSPORTBOB MAHSULOTLAR O`RGANILDI.</a>
+                        </li>
+                        <li>
+                            <a href="https://depstan.uz/2022/08/09/xalqaro-ekspertlar-boshchiligida-treyning-seminar/">XALQARO EKSPERTLAR BOSHCHILIGIDA TREYNING SEMINAR&#8230;</a>
+                        </li>
+                    </ul>
+
+                </aside>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+@include('partials.footer')
+@include('partials.script')
+</body>
+
+</html>
