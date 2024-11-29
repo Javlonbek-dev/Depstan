ac468c7a355466f0c3e129271aa129ddcfad750a
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-11-29 23:01:31 +0500

    code rub

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1218,9 +1218,9 @@
             }
         }
     </style>
-    <link rel="icon" href="{{ Vite::asset('resources/images/3.png') }}" sizes="32x32"/>
-    <link rel="icon" href="{{ Vite::asset('resources/images/4.png') }}" sizes="192x192"/>
-    <link rel="apple-touch-icon" href="{{ Vite::asset('resources/images/5.png') }}"/>
+{{--    <img src="{{ Vite::asset('resources/images/3.png') }}" alt="Image">--}}
+{{--    <link rel="icon" href="{{ Vite::asset('resources/images/4.png') }}" sizes="192x192"/>--}}
+{{--    <link rel="apple-touch-icon" href="{{ Vite::asset('resources/images/5.png') }}"/>--}}
     <meta name="msapplication-TileImage"
           content="{{ Vite::asset('resources/images/6.png') }}"/>
     <style id="wp-custom-css">
